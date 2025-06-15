f28b54bc85bd579e47a58d74987b0694209165e3
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-06-15 23:02:39 +0500

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
   private static String msg = "Hello and welcome qa guru!!!!!"
   public static void main(String[] args) {
-        System.out.printf(msg);
+        System.out.println(msg);
         }
     }
